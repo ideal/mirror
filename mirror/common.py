6953c6999856e2960abdeed57f22a52da29ff85b
ideal <idealities@gmail.com>
2013-06-19 22:23:53 +0800

    add common.py

---
+++ mirror/common.py
@@ -0,0 +1,72 @@
+#
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# mirror is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mirror. If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301, USA.
+#
+#    In addition, as a special exception, the copyright holders give
+#    permission to link the code of portions of this program with the OpenSSL
+#    library.
+#    You must obey the GNU General Public License in all respects for all of
+#    the code used other than OpenSSL. If you modify file(s) with this
+#    exception, you may extend this exception to your version of the file(s),
+#    but you are not obligated to do so. If you do not wish to do so, delete
+#    this exception statement from your version. If you delete this exception
+#    statement from all source files in the program, then also delete it here.
+#
+#
+
+
+"""Common functions for Mirror :("""
+
+import os, sys
+import time
+import logging
+import pkg_resources
+import gettext
+import locale
+
+from mirror.error import *
+
+log = logging.getLogger(__name__)
+
+def get_version():
+    """
+    Returns the version of mirror from the python egg metadata
+
+    :returns: the version of Deluge
+
+    """
+    return pkg_resources.require("mirror")[0].version
+
+def get_default_config_dir(filename=None):
+    """
+    :param filename: if None, only the config directory path is returned,
+                     if provided, a path including the filename will be returned
+    :type  filename: string
+    :returns: a file path to the config directory and optional filename
+    :rtype: string
+
+    """
+
+    from xdg.BaseDirectory import save_config_path
+    if not filename:
+        filename = ''
+    try:
+        return os.path.join(save_config_path("mirror"), filename)
+    except OSError, e:
+        log.error("Unable to use default config directory, exiting... (%s)", e)
+        sys.exit(1)
+
