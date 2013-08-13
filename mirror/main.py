1fe8ea2246b68c4e2ad1b3a80527b22f0b97f2d3
ideal <idealities@gmail.com>
2013-08-13 22:23:47 +0800

    update...

--- mirror/main.py
+++ mirror/main.py
@@ -84,7 +84,8 @@ def start_daemon():
     # Sets the options.logfile to point to the default location
     def set_logfile():
         if not options.logfile:
-            options.logfile = os.path.join(mirror.common.DEFAULT_MIRRORD_LOG_DIR, "mirrord.log")
+            options.logfile = os.path.join(mirror.common.DEFAULT_MIRRORD_LOG_DIR,
+                                           "mirrord.log")
 
     set_logfile()
 
