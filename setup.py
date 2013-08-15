1354a2fa28300790ac9d46b4f7740d7a1a107fac
ideal <idealities@gmail.com>
2013-08-15 17:56:38 +0800

    change version to 0.6.1

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ entry_points = {
 # Main setup
 setup(
     name = "mirror",
-    version = "0.6.0",
+    version = "0.6.1",
     fullname = "Mirror for open source mirror site",
     description = "Mirror for open source mirror site to sync files",
     author = "Shang Yuanchun",
