fef41f0279899f2016947d494a9a4156ec19997e
ideal <idealities@gmail.com>
2013-10-17 10:33:27 +0800

    add files for plugins

---
+++ mirror/pluginbase.py
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2013 Shang Yuanchun <idealities@gmail.com>
+#
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# mirror is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mirror. If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301, USA.
+#
+#
+
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class PluginBase(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def enable(self):
+        raise NotImplementedError("Need to define an enable method!")
+
+    def disable(self):
+        raise NotImplementedError("Need to define a disable method!")
+
