efb29dec1f29ccb919f867c845ad766499466c70
ideal <idealities@gmail.com>
2013-10-29 11:45:59 +0800

    for plugins

---
+++ mirror/plugins/SlateFish/mirror/plugins/slatefish/plugin.py
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2013 Shang Yuanchun <idealities@gmail.com>
+#
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# mirror is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mirror. If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301, USA.
+#
+#
+
+from mirror.pluginbase import PluginBase
+
+class Plugin(PluginBase):
+    
+    def enable(self):
+        pass
+
+    def disable(self):
+        pass
