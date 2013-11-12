e406f2c2356cbd0322cb82d3a88ea207971b8170
ideal <idealities@gmail.com>
2013-11-12 15:51:24 +0800

    add plugin

---
+++ mirror/plugins/TaskStatus/mirror/plugins/taskstatus/plugin.py
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2013 Shang Yuanchun <idealities@gmail.com>
+#
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# mirror is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mirror. If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301, USA.
+#
+#
+
+import json
+import logging
+import mirror.component as component
+from mirror.pluginbase import PluginBase
+
+log = logging.getLogger("taskstatus")
+
+class Plugin(PluginBase):
+
+    def enable(self):
+        plugin_manager = component.get("PluginManager")
+        config = plugin_manager.config
+
+        event_manager  = component.get("EventManager")
+        event_manager.register_event_handler("TaskStartEvent",
+                                             self.__on_task_start)
+        event_manager.register_event_handler("TaskStopEvent",
+                                             self.__on_task_stop)
+
+    def disable(self):
+        pass
+
+    def __on_task_start(self, taskname, pid):
+        pass
+
+    def __on_task_stop(self, taskname, pid, exitcode):
+        pass
---
+++ mirror/plugins/TaskStatus/setup.py
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2013 Shang Yuanchun <idealities@gmail.com>
+#
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# mirror is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mirror. If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301, USA.
+#
+#
+
+from setuptools import setup, find_packages
+
+__plugin_name__  = "TaskStatus"
+__author__       = "Shang Yuanchun"
+__author_email__ = "idealities@gmail.com"
+__version__      = "0.6"
+__url__          = "http://mirror.bjtu.edu.cn"
+__license__      = "GPLv3"
+__description__  = "Export task status to somewhere."
+__long_description__ = """"""
+__pkg_data__     = { 'mirror.plugins.' + __plugin_name__.lower(): [ "data/*" ] }
+
+setup(
+    name=__plugin_name__,
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    license=__license__,
+    long_description=__long_description__ if __long_description__ else __description__,
+    packages=find_packages(),
+    namespace_packages = ["mirror", "mirror.plugins"],
+    package_data = __pkg_data__,
+
+    entry_points="""
+    [mirror.plugin.mirrorplugin]
+    %s = mirror.plugins.%s:MirrorPlugin
+    """ % (__plugin_name__, __plugin_name__.lower())
+)
