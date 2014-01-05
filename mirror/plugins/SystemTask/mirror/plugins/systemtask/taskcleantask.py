a948866bbadf458469efc677278e0262c44e0def
ideal <idealities@gmail.com>
2014-01-05 22:27:16 +0800

    add task cleaner

---
+++ mirror/plugins/SystemTask/mirror/plugins/systemtask/taskcleantask.py
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2013 Shang Yuanchun <idealities@gmail.com>
+#
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# mirror is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mirror. If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA  02110-1301, USA.
+#
+#
+
+import os
+import time
+import signal
+import logging
+import mirror.component as component
+from mirror.task import SystemTask
+
+_name = "taskcleaner"
+
+log = logging.getLogger(_name)
+
+class TaskCleanTask(SystemTask):
+
+    # For those tasks which have no time out set,
+    # we will check it after 20 days.
+    TASK_TIMEOUT = 1728000
+
+    def __init__(self):
+        # Actually the `priority` here is meaningless
+        taskinfo = { "time": "0 2 * * *", "priority": 6 }
+        super(TaskCleanTask, self).__init__(_name, None, **taskinfo)
+
+        self.timeout_days = self.TASK_TIMEOUT / (24 * 3600)
+
+    def run(self):
+        scheduler = component.get("Scheduler")
+
+        curtime   = time.time()
+        for taskname, task in scheduler.tasks:
+            try:
+                if not task.running:
+                    continue
+                # SIGCHLD will trigger in main thread
+                if curtime - task.start_time > self.TASK_TIMEOUT:
+                    os.kill(task.pid, os.SIGTERM)
+            except Exception, e:
+                log.exception(e)
+            else:
+                log.info("Killed task: %s, whose life exceeds %d days",
+                         taskname, self.timeout_days)
+
