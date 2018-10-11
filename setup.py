e1ae350a8c4286f117e6d479f7de27d2ea7261d6
ideal <idealities@gmail.com>
2018-10-11 20:18:14 +0800

    add classifiers

--- setup.py
+++ setup.py
@@ -205,5 +205,16 @@ setup(
                                ]},
     packages = find_packages(exclude=["docs", "test"]),
     python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
     entry_points = entry_points
 )
