61dcc7561067ed5e0c29144a9e2a2e6a45d4dc80
ideal <idealities@gmail.com>
2018-10-14 11:59:35 +0800

    update version to 0.7.8

--- setup.py
+++ setup.py
@@ -188,7 +188,7 @@ entry_points = {
 # Main setup
 setup(
     name = "mirror",
-    version = "0.7.6",
+    version = "0.7.8",
     fullname = "Mirror for open source mirror site",
     description = "Mirror for open source mirror site to sync files",
     author = "Shang Yuanchun, Bob Gao",
