15bed4241593abcd11d77652803f68b977c4b810
ideal <idealities@gmail.com>
2020-04-29 00:49:39 +0800

    ok for 3.8

--- setup.py
+++ setup.py
@@ -220,6 +220,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points = entry_points
 )
